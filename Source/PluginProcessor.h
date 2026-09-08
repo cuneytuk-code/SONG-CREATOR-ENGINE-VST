fe93d9f18e04e5018cd7e5e50373217c7b9c2381
cuneytuk-code <cuneyt.uk@gmail.com>
2026-09-08 19:54:09 +0100

    Update PluginProcessor.h

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,31 +1,15 @@
 #pragma once
-#include <juce_audio_processors/juce_audio_processors.h>
+#include "PluginProcessor.h"
+#include <juce_gui_extra/juce_gui_extra.h>
 
-class MusicStudioAudioProcessor : public juce::AudioProcessor
+class MusicStudioAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    MusicStudioAudioProcessor();
-    ~MusicStudioAudioProcessor() override;
-
-    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
-    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
-    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
-    juce::AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
-    const juce::String getName() const override;
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram(int index) override;
-    const juce::String getProgramName(int index) override;
-    void changeProgramName(int index, const juce::String& newName) override;
-    void getStateInformation(juce::MemoryBlock& destData) override;
-    void setStateInformation(const void* data, int sizeInBytes) override;
-
+    explicit MusicStudioAudioProcessorEditor(MusicStudioAudioProcessor&);
+    ~MusicStudioAudioProcessorEditor() override;
+    void paint(juce::Graphics&) override;
+    void resized() override;
 private:
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MusicStudioAudioProcessor)
+    juce::WebBrowserComponent webView { juce::WebBrowserComponent::Options{}.withBackend(juce::WebBrowserComponent::Options::Backend::webview2).withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2{}.withUserDataFolder(juce::File::getSpecialLocation(juce::File::tempDirectory))) };
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MusicStudioAudioProcessorEditor)
 };
