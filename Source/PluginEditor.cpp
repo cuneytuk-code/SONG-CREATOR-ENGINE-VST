c937656e281bbd05b18662b9a12a4350693bb999
cuneytuk-code <cuneyt.uk@gmail.com>
2026-09-08 19:08:45 +0100

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,11 +1,14 @@
 #include "PluginEditor.h"
+#include "BinaryData.h"
 
 MusicStudioAudioProcessorEditor::MusicStudioAudioProcessorEditor(MusicStudioAudioProcessor& p)
     : AudioProcessorEditor(&p)
 {
-    setSize(1200, 800);
+    setSize(1400, 900);
     addAndMakeVisible(webView);
-    webView.goToURL("https://www.google.com");
+    auto html = juce::String::fromUTF8(BinaryData::index_html, BinaryData::index_htmlSize);
+    auto url = juce::String("data:text/html,") + juce::URL::addEscapeChars(html, false);
+    webView.goToURL(url);
 }
 MusicStudioAudioProcessorEditor::~MusicStudioAudioProcessorEditor() {}
 void MusicStudioAudioProcessorEditor::paint(juce::Graphics& g){ g.fillAll(juce::Colours::black); }
