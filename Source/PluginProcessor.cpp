41036c20f142aa2ffef0675afa952d9e2212e60e
cuneytuk-code <cuneyt.uk@gmail.com>
2026-09-08 20:52:35 +0100

    Add files via upload

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,18 +1,14 @@
+#include "PluginProcessor.h"
 #include "PluginEditor.h"
-#include "BinaryData.h"
 
-MusicStudioAudioProcessorEditor::MusicStudioAudioProcessorEditor(MusicStudioAudioProcessor& p)
-    : AudioProcessorEditor(&p)
+MusicStudioAudioProcessor::MusicStudioAudioProcessor()
+    : AudioProcessor(BusesProperties().withOutput("Output", juce::AudioChannelSet::stereo(), true))
 {
-    setSize(1400, 900);
-    setResizable(true, true);
-    addAndMakeVisible(webView);
-    
-    // Load the real HTML from BinaryData as data URL
-    juce::String html = juce::String::fromUTF8(BinaryData::index_html, BinaryData::index_htmlSize);
-    webView.goToURL("data:text/html;base64," + juce::Base64::toBase64(html.toRawUTF8(), html.getNumBytesAsUTF8()));
 }
 
-MusicStudioAudioProcessorEditor::~MusicStudioAudioProcessorEditor() {}
-void MusicStudioAudioProcessorEditor::paint(juce::Graphics& g){ g.fillAll(juce::Colours::black); }
-void MusicStudioAudioProcessorEditor::resized(){ webView.setBounds(getLocalBounds()); }
+MusicStudioAudioProcessor::~MusicStudioAudioProcessor() {}
+
+juce::AudioProcessorEditor* MusicStudioAudioProcessor::createEditor()
+{
+    return new MusicStudioAudioProcessorEditor(*this);
+}
