9abad20e6b8f210f4f4608a9c54d188c49b47db9
cuneytuk-code <cuneyt.uk@gmail.com>
2026-09-08 20:13:43 +0100

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,11 +1,11 @@
 #include "PluginEditor.h"
+
 MusicStudioAudioProcessorEditor::MusicStudioAudioProcessorEditor(MusicStudioAudioProcessor& p)
     : AudioProcessorEditor(&p)
 {
-    setSize(1400, 900);
-    setResizable(true, true);
+    setSize(1200, 800);
     addAndMakeVisible(webView);
-    webView.goToURL(webView.getResourceProviderRoot());
+    webView.goToURL("https://www.google.com");
 }
 MusicStudioAudioProcessorEditor::~MusicStudioAudioProcessorEditor() {}
 void MusicStudioAudioProcessorEditor::paint(juce::Graphics& g){ g.fillAll(juce::Colours::black); }
