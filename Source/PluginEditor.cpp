5da8e0751b5eec85ca0fff234d8e3d51829e8796
cuneytuk-code <cuneyt.uk@gmail.com>
2026-09-08 18:33:58 +0100

    Add files via upload

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,44 +1,41 @@
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "BinaryData.h"
 
 MusicStudioAudioProcessorEditor::MusicStudioAudioProcessorEditor(MusicStudioAudioProcessor& p)
-    : AudioProcessorEditor(&p)
+    : AudioProcessorEditor(&p), webView(juce::WebBrowserComponent::Options{}.withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
+        .withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2{}.withUserDataFolder(juce::File::getSpecialLocation(juce::File::tempDirectory)))
+        .withResourceProvider([](const juce::String& url) -> std::optional<juce::WebBrowserComponent::Resource> {
+            if (url == "/" || url == "/index.html") {
+                auto data = BinaryData::getNamedResource(BinaryData::namedResourceList[0], BinaryData::namedResourceListSize[0]);
+                // Actually get index.html
+                for (int i=0;i<BinaryData::namedResourceListSize;++i){
+                    juce::String name(BinaryData::namedResourceList[i]);
+                    if (name.contains("index")){
+                        int size=0;
+                        auto res = BinaryData::getNamedResource(BinaryData::namedResourceList[i], size);
+                        juce::String html = juce::String::fromUTF8((const char*)res, size);
+                        std::vector<std::byte> bytes;
+                        bytes.reserve(html.getNumBytesAsUTF8());
+                        for (auto c: html.toRawUTF8()) bytes.push_back((std::byte)c);
+                        // Simpler: return string resource via lambda that loads file
+                        // JUCE expects vector<byte> for content
+                        std::vector<std::byte> content;
+                        auto* src = res;
+                        for(int j=0;j<size;++j) content.push_back((std::byte)src[j]);
+                        return juce::WebBrowserComponent::Resource{ std::move(content), "text/html" };
+                    }
+                }
+                return std::nullopt;
+            }
+            return std::nullopt;
+        })
+        .withNativeIntegrationEnabled())
 {
     setSize(1400, 900);
     setResizable(true, true);
-    setResizeLimits(1200, 700, 3000, 2000);
-
     addAndMakeVisible(webView);
-
-    // Load HTML - during development load from Resources folder
-    auto htmlFile = juce::File::getCurrentWorkingDirectory().getChildFile("Resources").getChildFile("index.html");
-    if (!htmlFile.existsAsFile())
-    {
-        // Try relative to binary
-        htmlFile = juce::File::getSpecialLocation(juce::File::currentApplicationFile)
-            .getParentDirectory().getChildFile("..").getChildFile("..").getChildFile("Resources").getChildFile("index.html");
-    }
-    
-    if (htmlFile.existsAsFile())
-    {
-        webView.goToURL(juce::WebBrowserComponent::getResourceProviderRoot() + "index.html");
-        // Alternative: webView.goToURL(htmlFile.getFullPathName());
-    }
-    else
-    {
-        // Fallback: load from embedded string - replace with your HTML
-        webView.goToURL(juce::WebBrowserComponent::getResourceProviderRoot());
-    }
+    webView.goToURL(webView.getResourceProviderRoot());
 }
-
 MusicStudioAudioProcessorEditor::~MusicStudioAudioProcessorEditor() {}
-
-void MusicStudioAudioProcessorEditor::paint(juce::Graphics& g)
-{
-    g.fillAll(juce::Colours::black);
-}
-
-void MusicStudioAudioProcessorEditor::resized()
-{
-    webView.setBounds(getLocalBounds());
-}
+void MusicStudioAudioProcessorEditor::paint(juce::Graphics& g){ g.fillAll(juce::Colours::black); }
+void MusicStudioAudioProcessorEditor::resized(){ webView.setBounds(getLocalBounds()); }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -7,41 +7,9 @@ class MusicStudioAudioProcessorEditor : public juce::AudioProcessorEditor
 public:
     explicit MusicStudioAudioProcessorEditor(MusicStudioAudioProcessor&);
     ~MusicStudioAudioProcessorEditor() override;
-
     void paint(juce::Graphics&) override;
     void resized() override;
-
 private:
-    juce::WebBrowserComponent webView{juce::WebBrowserComponent::Options{}
-        .withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
-        .withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2{}
-            .withUserDataFolder(juce::File::getSpecialLocation(juce::File::tempDirectory))
-        )
-        .withResourceProvider([this](const juce::String& url) -> std::optional<juce::WebBrowserComponent::Resource>
-        {
-            if (url == "/" || url == "/index.html")
-            {
-                // Load embedded HTML from binary data
-                extern const char* index_html;
-                extern const int index_htmlSize;
-                // Fallback: load from Resources folder during dev
-                auto file = juce::File::getCurrentWorkingDirectory().getChildFile("Resources").getChildFile("index.html");
-                if (!file.existsAsFile())
-                    file = juce::File::getSpecialLocation(juce::File::currentApplicationFile)
-                        .getParentDirectory().getChildFile("Resources").getChildFile("index.html");
-                
-                if (file.existsAsFile())
-                {
-                    return juce::WebBrowserComponent::Resource{ file.loadFileAsString().toRawUTF8(), "text/html" };
-                }
-                
-                // Embedded fallback - will be replaced by binary data
-                return std::nullopt;
-            }
-            return std::nullopt;
-        })
-        .withNativeIntegrationEnabled()
-    };
-
+    juce::WebBrowserComponent webView;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MusicStudioAudioProcessorEditor)
 };
