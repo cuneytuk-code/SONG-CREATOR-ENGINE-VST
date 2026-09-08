e7e164c9113425d2f0cc8893c90afc5a548f352b
cuneytuk-code <cuneyt.uk@gmail.com>
2026-09-08 19:45:41 +0100

    Update PluginProcessor.cpp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,27 +1,18 @@
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "BinaryData.h"
 
-MusicStudioAudioProcessor::MusicStudioAudioProcessor()
-    : AudioProcessor(BusesProperties().withOutput("Output", juce::AudioChannelSet::stereo(), true)) {}
-MusicStudioAudioProcessor::~MusicStudioAudioProcessor() {}
-const juce::String MusicStudioAudioProcessor::getName() const { return "Music Studio"; }
-bool MusicStudioAudioProcessor::acceptsMidi() const { return true; }
-bool MusicStudioAudioProcessor::producesMidi() const { return false; }
-bool MusicStudioAudioProcessor::isMidiEffect() const { return false; }
-double MusicStudioAudioProcessor::getTailLengthSeconds() const { return 0.0; }
-int MusicStudioAudioProcessor::getNumPrograms() { return 1; }
-int MusicStudioAudioProcessor::getCurrentProgram() { return 0; }
-void MusicStudioAudioProcessor::setCurrentProgram(int) {}
-const juce::String MusicStudioAudioProcessor::getProgramName(int) { return {}; }
-void MusicStudioAudioProcessor::changeProgramName(int, const juce::String&) {}
-void MusicStudioAudioProcessor::prepareToPlay(double, int) {}
-void MusicStudioAudioProcessor::releaseResources() {}
-bool MusicStudioAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const {
-    return layouts.getMainOutputChannelSet() == juce::AudioChannelSet::stereo();
+MusicStudioAudioProcessorEditor::MusicStudioAudioProcessorEditor(MusicStudioAudioProcessor& p)
+    : AudioProcessorEditor(&p)
+{
+    setSize(1400, 900);
+    setResizable(true, true);
+    addAndMakeVisible(webView);
+    
+    // Load the real HTML from BinaryData as data URL
+    juce::String html = juce::String::fromUTF8(BinaryData::index_html, BinaryData::index_htmlSize);
+    webView.goToURL("data:text/html;base64," + juce::Base64::toBase64(html.toRawUTF8(), html.getNumBytesAsUTF8()));
 }
-void MusicStudioAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer&) { buffer.clear(); }
-juce::AudioProcessorEditor* MusicStudioAudioProcessor::createEditor() { return new MusicStudioAudioProcessorEditor(*this); }
-bool MusicStudioAudioProcessor::hasEditor() const { return true; }
-void MusicStudioAudioProcessor::getStateInformation(juce::MemoryBlock&) {}
-void MusicStudioAudioProcessor::setStateInformation(const void*, int) {}
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() { return new MusicStudioAudioProcessor(); }
+
+MusicStudioAudioProcessorEditor::~MusicStudioAudioProcessorEditor() {}
+void MusicStudioAudioProcessorEditor::paint(juce::Graphics& g){ g.fillAll(juce::Colours::black); }
+void MusicStudioAudioProcessorEditor::resized(){ webView.setBounds(getLocalBounds()); }
