843db5fc3ff46a8746c0f5b383212a5538840d7d
cuneytuk-code <cuneyt.uk@gmail.com>
2026-09-08 19:44:47 +0100

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,21 +10,6 @@ public:
     void paint(juce::Graphics&) override;
     void resized() override;
 private:
-    juce::WebBrowserComponent webView { juce::WebBrowserComponent::Options{}
-       .withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
-       .withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2{}.withUserDataFolder(juce::File::getSpecialLocation(juce::File::tempDirectory)))
-       .withResourceProvider([](const juce::String& url) -> std::optional<juce::WebBrowserComponent::Resource> {
-            if (url == "/" || url.contains("index.html")) {
-                auto* data = BinaryData::index_html;
-                int size = BinaryData::index_htmlSize;
-                std::vector<std::byte> bytes;
-                bytes.reserve(size);
-                for (int i = 0; i < size; ++i) bytes.push_back(static_cast<std::byte>(data[i]));
-                return juce::WebBrowserComponent::Resource{ std::move(bytes), "text/html" };
-            }
-            return std::nullopt;
-        })
-       .withNativeIntegrationEnabled()
-    };
+    juce::WebBrowserComponent webView { juce::WebBrowserComponent::Options{}.withBackend(juce::WebBrowserComponent::Options::Backend::webview2).withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2{}.withUserDataFolder(juce::File::getSpecialLocation(juce::File::tempDirectory))) };
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MusicStudioAudioProcessorEditor)
 };
